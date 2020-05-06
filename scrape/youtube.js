df59b72c1182c0c3e1b95c08ff5d35002242830d
meganniu <megan.niu@gmail.com>
2020-05-06 19:21:52 -0400

    style(youtube): remove comments

--- scrape/youtube.js
+++ scrape/youtube.js
@@ -9,15 +9,13 @@ const scrape = async (videoURL) => {
   youtubeAPIURL.searchParams.append("id", parsedURL.searchParams.get("v"));
   youtubeAPIURL.searchParams.append("part", "snippet");
   youtubeAPIURL.searchParams.append("key", process.env.GOOGLE_API_KEY);
-  // console.log(youtubeAPIURL.toString());
-  // console.log(youtubeAPIURL.toString());
+  
   let data = null;
   await axios({
     method: "get",
     url: youtubeAPIURL.toString(),
   })
     .then((response) => {
-      // console.log(response.data.items[0]);
       data = response.data.items[0];
     })
     .catch((err) => {
@@ -32,9 +30,7 @@ const scrape = async (videoURL) => {
     ret.push(`${data.snippet.title}`);
     ret.push(`${data.snippet.description} `);
   }
-  // console.log(data.snippet.tags);
-  // console.log(JSON.parse(data.snippet.tags));
-  // console.log(ret);
+  
   return ret;
 };
 
