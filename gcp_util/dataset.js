c0e51e1c2b8ca566dc2fba6df920baeefddaacd1
meganniu <megan.niu@gmail.com>
2020-05-06 20:59:01 -0400

    feat(dataset): add item import callback to create dataset promise

--- gcp_util/dataset.js
+++ gcp_util/dataset.js
@@ -5,69 +5,90 @@ const { AutoMlClient } = require("@google-cloud/automl").v1;
 const client = new AutoMlClient();
 
 class Dataset {
-  constructor(projectId, location, displayName) {
+  constructor(projectId, location, datasetName, datasetId) {
     this.projectId = projectId;
     this.location = location;
-    this.displayName = displayName;
-    this.name = null;
-    this.id = null;
-    this.created = false;
+    this.name = datasetName;
+    this.creationInProgress = false;
+    if (datasetId === undefined) {
+      this.id = null;
+      this.createDataset();
+    } else {
+      // TODO: check that dataset wit id actually exists
+      this.id = datasetId;
+      this.creationPromise = Promise.resolve(); //TODO: assign a dummy resolved promise
+    }
   }
-  async createDataSet() {
-    if (this.created === false) {
+
+  async createDataset() {
+    if (this.id === null && this.creationInProgress === false) {
+      this.creationInProgress = true;
       // Construct request
       const request = {
         parent: client.locationPath(this.projectId, this.location),
         dataset: {
-          displayName: this.displayName,
+          displayName: this.name,
           textClassificationDatasetMetadata: {
             classificationType: "MULTICLASS",
           },
         },
       };
 
-      // Create dataset
-      const [operation] = await client.createDataset(request);
+      this.creationPromise = client
+        .createDataset(request)
+        .then(([operation]) => operation.promise())
+        .then(([response]) => {
+          this.id = response.name
+            .split("/")
+            [response.name.split("/").length - 1].split("\n")[0];
+          this.creationInProgress = false;
 
-      // Wait for operation to complete.
-      const [response] = await operation.promise();
-
-      this.name = response.name;
-      this.id = response.name
-        .split("/")
-        [response.name.split("/").length - 1].split("\n")[0];
-      this.created = true;
-
-      console.log(`Dataset name: ${this.name}`);
-      console.log(`Dataset id: ${this.id}`);
+          console.log(`Dataset name: ${this.name}`);
+          console.log(`Dataset id: ${this.id}`);
+        });
     } else {
-      console.log(`Dataset ${this.name} has already been created.`);
+      if (this.creationInProgress === true) {
+        console.log(`Dataset ${this.name} (ID: ${this.id}) is being created.`);
+      } else {
+        console.log(
+          `Dataset ${this.name} (ID: ${this.id}) has already been created.`
+        );
+      }
     }
   }
 
-  async uploadDataSetItems(bucketName, fileName) {
-    const path = `gs://${bucketName}/${fileName}`;
-    const request = {
-      name: client.datasetPath(this.projectId, this.location, this.id),
-      inputConfig: {
-        gcsSource: {
-          inputUris: path.split(","),
+  async importDatasetItems(bucketName, fileName) {
+    this.creationPromise.then(async () => {
+      const path = `gs://${bucketName}/${fileName}`;
+      const request = {
+        name: client.datasetPath(this.projectId, this.location, this.id),
+        inputConfig: {
+          gcsSource: {
+            inputUris: path.split(","),
+          },
         },
-      },
-    };
+      };
 
-    // Import dataset
-    console.log("Proccessing import");
-    const [operation] = await client.importData(request);
+      // Import dataset
+      console.log("Proccessing import");
+      const [operation] = await client.importData(request);
 
-    // Wait for operation to complete.
-    const [response] = await operation.promise();
-    console.log("Dataset imported:", response);
+      // Wait for operation to complete.
+      const [response] = await operation.promise();
+      console.log(`Dataset imported: ${response}`);
+    });
   }
 
   getId() {
     return this.id;
   }
 }
+
+// const test = () => {
+//   dataset = new Dataset("softblocker", "us-central1", "test_4");
+//   dataset.importDatasetItems("softblocker-lcm", "Homer.csv");
+// };
+
+// test();
 
 module.exports = Dataset;
