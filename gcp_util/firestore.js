d050f7dc02a7a45117375f3893482bbd20a0989b
meganniu <megan.niu@gmail.com>
2020-05-08 00:02:18 -0400

    feat(firestore): create API wrapper for Firestore

--- gcp_util/firestore.js
+++ gcp_util/firestore.js
@@ -14,8 +14,22 @@ class Collection {
 
   async getDoc(docId) {
     let docData = null;
-    await this.collectionRef.doc(docId).get().then((docRef) => docData = docRef.data());
+    await this.collectionRef
+      .doc(docId)
+      .get()
+      .then((docRef) => (docData = docRef.data()));
     return docData;
+  }
+
+  async writeDoc(docId, docData) {
+    // If merge = true, existing fields in doc preserved and docData replaces conflicting fields and adds new fields.
+    // If merge = false or unset, existing fields are all deleted and replaced with docData.
+    await this.collectionRef
+      .doc(docId)
+      .set(docData, { merge: true })
+      .catch((err) => {
+        throw err;
+      });
   }
 }
 
@@ -24,6 +38,10 @@ class Collection {
 //   const docId = "f0NMjg0XLjQAiZzJvIE9";
 //   const collection = new Collection(collectionName);
 
+//   console.log(await collection.getDoc(docId));
+//   await collection.writeDoc(docId, {
+//     labels: ["Homer"],
+//   });
 //   console.log(await collection.getDoc(docId));
 // };
 
